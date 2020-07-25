d312f15654d48194e5865af550387c106e593ad7
Girla <girish.la@gmail.com>
2020-07-25 19:23:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,19 +14,18 @@ function installPMD(){
   var mv = 'mv /tmp/pmd-bin-6.19.0/* $HOME/pmd'
   exec(download+' && '+unzip+' && '+mk+' && '+mv, function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
-    core.debug(stdout)
     referencePMD()
   })
 }
 
 function referencePMD(){
-  var mk = 'sudo mkdir /snap/bin && sudo chmod -R 757 /snap/bin'
+  var mk = 'sudo mkdir -p /snap/bin && sudo chmod -R 757 /snap/bin'
   var cmd = 
 `sudo echo '#! /bin/bash
 $HOME/pmd/bin/run.sh pmd "$@"' > /snap/bin/pmd`
   var cm = 'chmod +x /snap/bin/pmd'
   exec(mk+' && '+cmd+' && '+cm, function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
-    core.debug(stdout)
+    
   })
 }
